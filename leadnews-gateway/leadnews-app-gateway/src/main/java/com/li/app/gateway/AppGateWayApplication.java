950963cb41ccc965032a8ca6dd25d7f94295d4d0
liyuelian <liyuelian.ke@outlook.com>
2024-07-11 01:11:42 +0800

    feat:新增微服务网关leadnews-gateway 1)子模块app-gateway网关实现 2)全局认证过滤器校验jwt

--- leadnews-gateway/leadnews-app-gateway/src/main/java/com/li/app/gateway/AppGateWayApplication.java
+++ leadnews-gateway/leadnews-app-gateway/src/main/java/com/li/app/gateway/AppGateWayApplication.java
@@ -1,15 +1,19 @@
-package com.app.gateway;
+package com.li.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @Author: liyuelian
+ * @Date: 2024/7/10 20:13
+ * @Description:
+ **/
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class AppGatewayApplication {
-
+@EnableDiscoveryClient //开启注册中心
+public class AppGateWayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AppGatewayApplication.class,args);
+        SpringApplication.run(AppGateWayApplication.class,args);
     }
 }
